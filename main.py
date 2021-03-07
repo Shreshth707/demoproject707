af39638f665071dc9288886e7a30fb97ccf20189
Shreshth <55982438+Shreshth707@users.noreply.github.com>
2021-03-08 01:44:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ app = FastAPI()
 async def root():
   return {"message": "Hello World"}
 
+
+
+@app.get('/recommendation')
+async def recommendation(title:str):
+	app.model = RecommenderEngine(title.lower());
+	recommendations = app.model.getRecommendations();
+	return recommendations;
+
+
+
 if __name__ == "__main__" :
   app.run(debug=True)
